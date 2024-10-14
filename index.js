b76aef73de2506327d1a899f547677321f0e1d4e
Muxsimov <muxsimovasadbek665@gmail.com>
2024-10-14 14:25:13 +0500

    finished

---
+++ index.js
@@ -0,0 +1,11 @@
+let name=prompt("Enter your name");
+let surname=prompt("Enter your surname");
+let age=prompt("Enter your age");
+let city=prompt("Enter your city");
+let gender=prompt("Enter your gender");
+let language=prompt("Enter your language");
+
+
+console.log(name,surname,"yoshi",age,"da","yashash joyi",city,"jinsi",gender,"gaplashish tili",language);
+
+
